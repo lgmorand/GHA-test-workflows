f23e76aeb50694497fc431ab4f6db2d4f97a90a5
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-04-04 18:05:21 +0200

    wip

---
+++ DemoNuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
